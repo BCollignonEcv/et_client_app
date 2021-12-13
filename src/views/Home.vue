96b8f3e056b6f103f07040b03264facf47e852e5
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2021-12-13 03:42:02 +0100

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="home">
+        <h1>et_client_app</h1>
+        <img alt="Vue logo" src="@/assets/et_solution-C4.png">
+    </div>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+#home{
+    width: 100%;
+    height: 100%;
+}
+
+img{
+    height: 80vh;
+    width: auto;
+}
+</style>
